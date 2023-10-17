64f1c5d4a4b085e0d4f5b97c0052621dab423489
Robert Leaf <robert.t.leaf@gmail.com>
2023-10-17 08:14:07 -0500

    update for second exam

--- bookdown_render_github.r
+++ bookdown_render_github.r
@@ -6,3 +6,10 @@ dir.create("_main_files/figure-html")
 
 file.copy(from = paste0("_bookdown_files/_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")),
           to = paste0("_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")))
+
+
+bookdown::render_book()
+for(i in dir("_book", pattern="html", full.names=TRUE)) {
+  x<-readLines(i)
+  writeLines(gsub("_main_files/", "", x), i)
+}
