8e3ea822d66aea18c1c7901e3d3a52aa107cdb89
Robert Leaf <robert.t.leaf@gmail.com>
2023-10-17 10:24:31 -0500

    test update

--- bookdown_render_github.r
+++ bookdown_render_github.r
@@ -1,11 +1,11 @@
 require(tidyverse)
-bookdown::render_book(output_dir = getwd())
-
-dir.create("_main_files")
-dir.create("_main_files/figure-html")
-
-file.copy(from = paste0("_bookdown_files/_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")),
-          to = paste0("_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")))
+# bookdown::render_book(output_dir = getwd())
+# 
+# dir.create("_main_files")
+# dir.create("_main_files/figure-html")
+# 
+# file.copy(from = paste0("_bookdown_files/_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")),
+#           to = paste0("_main_files/figure-html/",dir("_bookdown_files/_main_files/figure-html")))
 
 
 bookdown::render_book()
